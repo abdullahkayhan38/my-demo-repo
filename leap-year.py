8b27d37d21353fc29f9889b1abe0d0bfe45ac74f
EmirhanImranTuzel <emrhntzl@gmail.com>
2021-11-23 20:56:52 +0300

    emirhan gonderdi

--- leap-year.py
+++ leap-year.py
@@ -2,7 +2,7 @@
 # To get year (integer input) from the user
 # This is my first explanation
 year = int(input("Enter a year: "))
-
+(emirhan)
 if (year % 4) == 0:
     if (year % 100) == 0:
         if (year % 400) == 0:
